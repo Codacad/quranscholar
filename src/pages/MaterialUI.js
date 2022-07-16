3c64a2b74fe1d400252a9dd5961308ceeedb3192
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-16 22:18:33 +0530

    started using material ui

---
+++ src/pages/MaterialUI.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const MaterialUI = () => {
+  return (
+    <div>
+        <Typography variant='h1'>
+          Material Ui
+        </Typography>
+    </div>
+  )
+}
+
+export default MaterialUI
