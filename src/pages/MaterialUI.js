ae2a53ad30f077469293a016ff0c688039f1c011
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-16 22:37:48 +0530

    added

--- src/pages/MaterialUI.js
+++ src/pages/MaterialUI.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Typography } from '@mui/material'
+import { Typography, Button } from '@mui/material'
 
 const MaterialUI = () => {
   return (
     <div>
-        <Typography variant='h1'>
-          Material Ui
+        <Typography variant='div' display='flex' flexDirection='column' justifyContent='center'>
+          <Typography 
+          variant='h1'
+          color="primary"
+          align='center'
+          >
+              Material Ui
+          </Typography>
+          <Typography 
+          noWrap
+          variant='h4'
+          color="secondary"
+          align='center'
+          >
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur suscipit iure nostrum fugiat soluta sit aliquam molestias officia tempora est odit itaque ut labore nobis ratione numquam modi cum rerum quis perspiciatis, fuga temporibus vitae autem iste. Recusandae quis deserunt atque facilis, harum quia ipsam enim impedit libero eos quo.
+          </Typography>
+          <Button variant="contained" color="secondary" width='50'>
+            Save Me
+          </Button>
         </Typography>
     </div>
   )
