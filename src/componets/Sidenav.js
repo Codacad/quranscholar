74bfb1d2c7be596a99304280b76f862441f0e636
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-17 09:18:07 +0530

    updated sidenav

--- src/componets/Sidenav.js
+++ src/componets/Sidenav.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Close from '../icons/close.svg'
 
 const Sidenav = () => {
+
+    const handleSidenavToggle = () => {
+        document.querySelector('.sidenav-wrapper').classList.add('sidenav-wrapper-toggle')
+        document.querySelector('.sidenav').classList.add('sidenav-toggle')
+    }
+
+
   return (
     <div className='sidenav-wrapper'>
         <div className="sidenav">
+            <div className="sidenav-header">
+                <h1><Link to="/" className='logo'>Quran Scholar</Link></h1>
+                <button onClick={() => handleSidenavToggle()}><img src={Close} alt="" /></button>
+            </div>
             <div className="contents">
-                <Link to="/" className='logo'>Quran Scholar</Link>
+                
                 <Link to="/admission" className='admission'>Admission</Link>
                 <ul>
                     <li><Link to="/">Home</Link></li>
