b58e59ef6f18ae743d2651d0dd8bdd88aeb1e32f
Mohd Rizwan <iamrizwan40@gmail.com>
2022-07-17 10:15:10 +0530

    deleted material-ui component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Navbar from './componets/Navbar';
 import Main from './pages/Main';
 import Courses from './pages/Courses';
-// import Sidenav from './componets/Sidenav';
-import MaterialUI from './pages/MaterialUI';
+import Sidenav from './componets/Sidenav';
 
 function App() {
   return (
     <div className="App">
         <Router>
           <Navbar />
-          {/* <Sidenav /> */}
+          <Sidenav />
           <Routes>
             <Route path='/' element={<Main />}></Route>
             <Route path='/courses' element={<Courses />}></Route>
-            <Route path='/materialui' element={<MaterialUI />}></Route>
           </Routes>
         </Router>
     </div>
